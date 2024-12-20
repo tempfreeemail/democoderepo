a8bcbf9f6cca010fb75c13d286c8b3f949b528ca
tempfreeemail <temp.free.email@gmail.com>
2024-12-20 16:54:53 +0530

    Addition of three numbers

---
+++ Program.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the first number : ");
+		int a = sc.nextInt();
+		
+		System.out.println("Enter the second number : ");
+		int b = sc.nextInt();
+		
+		System.out.println("Enter the second number : ");
+		int c = sc.nextInt();
+		int d = a + b + c;
+		
+		System.out.println("Addition : "+d);
+	}
+
+}
